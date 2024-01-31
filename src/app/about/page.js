73d9fc7b4bfab3b41f86ed596abd06c4d332368d
Steve Munoz <stevomunoz@yahoo.com>
2024-01-31 13:11:49 -0600

    created home about and van pages along with navbar

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+
+import Link from 'next/link'
+
+export default function About() {
+    return (
+      <>
+      <h1>about page</h1>
+      <Link href="/"> Return to Home Page </Link>
+      </>
+   
+    );
+  }
+  
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+      <>
+
+       
+       <Link href="/"> #VANLIFE</Link>
+       <Link href="/about"> About</Link>
+       <Link href="/vans"> Vans</Link>
+      
+      </>
+   
+    );
+  }
+  
