e390fd9366aa08491d82c3ae95ebe33d7f5709c2
Steve Munoz <stevomunoz@yahoo.com>
2024-02-26 12:00:39 -0600

    created Footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className ="bg-black h-14 inset-x-0 bottom-0 fixed">
+        <h1 className = "text-white mt-auto text-center p-4"> &#169; 2022 #VANLIFE</h1>
+      
+    </div>
+  );
+}
+
+export default Footer;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 import "./server"
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
      
-      <body className={inter.className}><Navbar/>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
+        <Footer/>
+
+      </body>
+      
       
     
     </html>
